19880c7edcfa75fd40bb10dc426e4a27ce6c9e67
TWO&solutions <59812127+TWOandSolutions@users.noreply.github.com>
2020-11-23 01:41:22 -0500

    Add files via upload

--- example_accuracy measures_2020a.py
+++ example_accuracy measures_2020a.py
@@ -5,21 +5,43 @@ Created on Fri Nov 20 22:45:16 2020
 @author: KevinKang from TWO & Solutions
 """
 
-from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn import datasets, linear_model
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
 # from goodness-of-fit as rmse
 
 
-data_sim = np.array([3, -0.5, 2, 7])
-data_obs = np.array([2.5, 0.0, 2, 8])
-
+# Preparing simulated and observed data
+data_sim = np.array([3, -0.5, 2, 7, 10, 4, 5.5, 8, 1.8, 7.5])
+data_obs = np.array([2.5, 0.0, 2, 8, 12, 3.5, 5, 9, 1.0, 8.5]).reshape((-1, 1))
 #data_sim = np.random.randint(0,100,size=10)
 #data_obs = np.random.randint(0,100,size=10)
+model = LinearRegression().fit(data_obs, data_sim)
 
+# Calculate RMSE, MBE, MAE, and Index of Agreement 
 print ("RMSE:" +str(np.sqrt(mean_squared_error(data_sim, data_obs))))
 print ("MBE: " +str(np.mean(data_sim-data_obs)))
 print ("MAE: " +str(np.mean(abs(data_sim-data_obs))))
 ia = (1 -(np.sum((data_obs-data_sim)**2))/(np.sum((np.abs(data_sim-np.mean(data_obs))+np.abs(data_obs-np.mean(data_obs)))**2)))
 print ("Ia: " +str(ia))
-    
+
+plt.scatter(data_sim, data_obs,  color='black')
+print ("R2: " +str(r2_score(data_obs, data_sim)))
+print ("intercept:", model.intercept_)
+print ("slope: ", model.coef_)       
+
+#plt.plot(data_obs, data_sim, color='blue', linewidth=3)
+#plt.xticks(())
+#plt.yticks(()) 
+#plt.show()   
+
+
+
+
+
+
 
