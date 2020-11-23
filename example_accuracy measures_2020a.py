a873294854bb8c4fad2f33e7dc75f11c83a4603c
TWO&solutions <59812127+TWOandSolutions@users.noreply.github.com>
2020-11-23 03:09:28 -0500

    Add files via upload

--- example_accuracy measures_2020a.py
+++ example_accuracy measures_2020a.py
@@ -12,18 +12,26 @@ from sklearn.metrics import mean_squared_error
 from sklearn import datasets, linear_model
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-
 # from goodness-of-fit as rmse
 
 
 # Preparing simulated and observed data
 data_sim = np.array([3, -0.5, 2, 7, 10, 4, 5.5, 8, 1.8, 7.5])
-#data_obs = np.array([2.5, 0.0, 2, 8, 12, 3.5, 5, 9, 1.0, 8.5]).reshape((-1, 1))
-data_obs = np.array([2.5, 0.0, 2, 8, 12, 3.5, 5, 9, 1.0, 8.5])
-
+data_obs = np.array([2.5, 0.0, 2, 8, 12, 3.5, 5, 9, 1.0, 8.5]).reshape((-1, 1))
 #data_sim = np.random.randint(0,100,size=10)
 #data_obs = np.random.randint(0,100,size=10)
+
+# Calculating determination of coefficient
 model = LinearRegression().fit(data_obs, data_sim)
+plt.scatter(data_sim, data_obs,  color='black')
+print ("R2: " +str(r2_score(data_obs, data_sim)))
+print ("intercept:", model.intercept_)
+print ("slope: ", model.coef_)
+
+
+# Preparing simulated and observed data
+data_sim = np.array([3, -0.5, 2, 7, 10, 4, 5.5, 8, 1.8, 7.5])
+data_obs = np.array([2.5, 0.0, 2, 8, 12, 3.5, 5, 9, 1.0, 8.5])
 
 # Calculate RMSE, MBE, MAE, and Index of Agreement 
 print ("RMSE:" +str(np.sqrt(mean_squared_error(data_sim, data_obs))))
@@ -32,18 +40,21 @@ print ("MAE: " +str(np.mean(abs(data_sim-data_obs))))
 ia = (1 -(np.sum((data_obs-data_sim)**2))/(np.sum((np.abs(data_sim-np.mean(data_obs))+np.abs(data_obs-np.mean(data_obs)))**2)))
 print ("Ia: " +str(ia))
 
+# Plotting
 plt.scatter(data_sim, data_obs,  color='black')
 print ("R2: " +str(r2_score(data_obs, data_sim)))
 print ("intercept:", model.intercept_)
 print ("slope: ", model.coef_)
 
+
+# Calculate pearson, spearman, kendall  
 scipy.stats.pearsonr(data_obs, data_sim)     # Pearson's r
 scipy.stats.spearmanr(data_obs, data_sim)    # Spearman's rho
 scipy.stats.kendalltau(data_obs, data_sim)   # Keindall's tau
 
-scipy.stats.pearsonr(data_obs, data_sim)[0]     # Pearson's r
-scipy.stats.spearmanr(data_obs, data_sim)[0]    # Spearman's rho
-scipy.stats.kendalltau(data_obs, data_sim)[0]   # Keindall's tau    
+print (scipy.stats.pearsonr(data_obs, data_sim)[0])     # Pearson's r
+print (scipy.stats.spearmanr(data_obs, data_sim)[0])    # Spearman's rho
+print (scipy.stats.kendalltau(data_obs, data_sim)[0])   # Keindall's tau    
 
 #plt.plot(data_obs, data_sim, color='blue', linewidth=3)
 #plt.xticks(())
