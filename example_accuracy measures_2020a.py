cea664eae955f802755091e5d5f81178d3df7eef
TWO&solutions <59812127+TWOandSolutions@users.noreply.github.com>
2020-11-23 02:53:11 -0500

    Add files via upload

--- example_accuracy measures_2020a.py
+++ example_accuracy measures_2020a.py
@@ -7,6 +7,7 @@ Created on Fri Nov 20 22:45:16 2020
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
 from sklearn.metrics import mean_squared_error
 from sklearn import datasets, linear_model
 from sklearn.linear_model import LinearRegression
@@ -17,7 +18,9 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 # Preparing simulated and observed data
 data_sim = np.array([3, -0.5, 2, 7, 10, 4, 5.5, 8, 1.8, 7.5])
-data_obs = np.array([2.5, 0.0, 2, 8, 12, 3.5, 5, 9, 1.0, 8.5]).reshape((-1, 1))
+#data_obs = np.array([2.5, 0.0, 2, 8, 12, 3.5, 5, 9, 1.0, 8.5]).reshape((-1, 1))
+data_obs = np.array([2.5, 0.0, 2, 8, 12, 3.5, 5, 9, 1.0, 8.5])
+
 #data_sim = np.random.randint(0,100,size=10)
 #data_obs = np.random.randint(0,100,size=10)
 model = LinearRegression().fit(data_obs, data_sim)
@@ -32,7 +35,15 @@ print ("Ia: " +str(ia))
 plt.scatter(data_sim, data_obs,  color='black')
 print ("R2: " +str(r2_score(data_obs, data_sim)))
 print ("intercept:", model.intercept_)
-print ("slope: ", model.coef_)       
+print ("slope: ", model.coef_)
+
+scipy.stats.pearsonr(data_obs, data_sim)     # Pearson's r
+scipy.stats.spearmanr(data_obs, data_sim)    # Spearman's rho
+scipy.stats.kendalltau(data_obs, data_sim)   # Keindall's tau
+
+scipy.stats.pearsonr(data_obs, data_sim)[0]     # Pearson's r
+scipy.stats.spearmanr(data_obs, data_sim)[0]    # Spearman's rho
+scipy.stats.kendalltau(data_obs, data_sim)[0]   # Keindall's tau    
 
 #plt.plot(data_obs, data_sim, color='blue', linewidth=3)
 #plt.xticks(())
